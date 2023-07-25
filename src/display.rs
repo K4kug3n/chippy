5fea5d38814ad8ea13b2889cd5672052747aa673
K4kug3n <kakugen@yahoo.fr>
2023-07-25 23:37:26 +0200

    Change internal screen representation

--- src/display.rs
+++ src/display.rs
@@ -1,31 +1,28 @@
-use bit_vec::BitVec;
-
 pub struct Display {
 	width: u8,
 	height: u8,
-	buffer: BitVec,
+	buffer: Vec<u8>,
 }
 
 impl Display {
 	pub fn new(width: u8, height: u8) -> Display {
+		debug_assert!(width % 8 == 0);
+		let buffer = vec![0; ((usize::from(width) * usize::from(height)) + 7) / 8];
+
 		Display {
 			width: width,
 			height: height,
-			buffer: BitVec::from_elem((width * height).into(), true)
+			buffer: buffer
 		}
 	}
 
 	pub fn clear(&mut self) {
-		self.buffer.set_all();
-
+		self.buffer = vec![0; self.buffer.len()];
 	}
 
-	pub fn flip(&mut self, x: u8, y: u8) {
+	pub fn draw(&mut self, x: u8, y: u8, sprite: &Vec<u8>) {
 		debug_assert!(x < self.width && y < self.height);
 
-		let index : usize = (y * self.width + x).into();
-		let old_value = self.buffer.get(index).unwrap();
-
-		self.buffer.set(index, !old_value);
+		
 	}
 }
