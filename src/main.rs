a0b1af23af8b6fd74c31474e700a738c30b5ce2c
K4kug3n <kakugen@yahoo.fr>
2023-07-26 20:10:13 +0200

    WIP window

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::io::prelude::*;
 use std::fs::File;
 
 use chippy::interpretor::Interpretor;
+use minifb::{Key, Window, WindowOptions};
 
 fn main() {
     let mut file = File::open("examples/1-chip8-logo.ch8").expect("Could not read the file {}");
@@ -23,6 +24,39 @@ fn main() {
 
     let mut interpretor = Interpretor::new(buffer);
 
-    interpretor.run();
+    let mut pixels: Vec<u32> = vec![0; interpretor.screen_width() * interpretor.screen_height()];
+    let width = interpretor.screen_width();
+    let height = interpretor.screen_height();
+
+    let mut window = Window::new(
+        "Test - ESC to exit",
+        width,
+        height,
+        WindowOptions::default(),
+    )
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        interpretor.cycle();
+
+        for y in 0..height {
+            for x in 0..width {
+                if interpretor.screen_value(x, y) != 0 {
+                    pixels[y * width + x] = 0x096096FF;
+                }
+                else {
+                    pixels[y * width + x] = 0x09609680;
+                }
+            }
+        }
+
+        window
+            .update_with_buffer(&pixels, width, height)
+            .unwrap();
+    }
 }
  
