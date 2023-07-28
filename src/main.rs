bfd52f6f3beb18c6a78812a9040d6d2d688c28a0
K4kug3n <kakugen@yahoo.fr>
2023-07-29 01:28:01 +0200

    Add timers and input + opcode associated

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,6 @@ fn main() {
     let mut buffer = Vec::new();
     file.read_to_end(&mut buffer).expect("Could not read bytes");
 
-    let mut program: Vec<u16> = Vec::new();
-    for i in (0..buffer.len()).step_by(2) {
-        let mut op : u16 = 0u16;
-        op += u16::from(buffer[i]) << 8;
-        op += u16::from(buffer[i+1]);
-
-        program.push(op)
-    }
-
-    println!("{:#06x?}", program);
-    println!("{}", program.len());
-
     let mut interpretor = Interpretor::new(buffer);
 
     const PIXEL_SIZE : usize = 16;
@@ -39,6 +27,30 @@ fn main() {
     });
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
+        interpretor.clear_inputs();
+
+        window.get_keys_pressed(minifb::KeyRepeat::No).iter().for_each(|key|
+            match key {
+                Key::NumPad0 => interpretor.set_pressed(0x0),
+                Key::NumPad1 => interpretor.set_pressed(0x1),
+                Key::NumPad2 => interpretor.set_pressed(0x2),
+                Key::NumPad3 => interpretor.set_pressed(0x3),
+                Key::NumPad4 => interpretor.set_pressed(0x4),
+                Key::NumPad5 => interpretor.set_pressed(0x5),
+                Key::NumPad6 => interpretor.set_pressed(0x6),
+                Key::NumPad7 => interpretor.set_pressed(0x7),
+                Key::NumPad8 => interpretor.set_pressed(0x8),
+                Key::NumPad9 => interpretor.set_pressed(0x9),
+                Key::A => interpretor.set_pressed(0xA),
+                Key::B => interpretor.set_pressed(0xB),
+                Key::C => interpretor.set_pressed(0xC),
+                Key::D => interpretor.set_pressed(0xD),
+                Key::E => interpretor.set_pressed(0xE),
+                Key::F => interpretor.set_pressed(0xF),
+                _ => {}
+            }
+        );
+
         interpretor.cycle();
 
         for y in 0..window_height {
