c0ea9e46fee70224841bc7e1863c17e32cbf2e31
K4kug3n <kakugen@yahoo.fr>
2023-07-29 01:54:44 +0200

    Better input management + 0xEX9E/0xEXA1 opcode

--- src/main.rs
+++ src/main.rs
@@ -27,8 +27,7 @@ fn main() {
     });
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        interpretor.clear_inputs();
-
+        
         window.get_keys_pressed(minifb::KeyRepeat::No).iter().for_each(|key|
             match key {
                 Key::NumPad0 => interpretor.set_pressed(0x0),
@@ -47,6 +46,28 @@ fn main() {
                 Key::D => interpretor.set_pressed(0xD),
                 Key::E => interpretor.set_pressed(0xE),
                 Key::F => interpretor.set_pressed(0xF),
+                _ => {}
+            }
+        );
+
+        window.get_keys_released().iter().for_each(|key|
+            match key {
+                Key::NumPad0 => interpretor.set_released(0x0),
+                Key::NumPad1 => interpretor.set_released(0x1),
+                Key::NumPad2 => interpretor.set_released(0x2),
+                Key::NumPad3 => interpretor.set_released(0x3),
+                Key::NumPad4 => interpretor.set_released(0x4),
+                Key::NumPad5 => interpretor.set_released(0x5),
+                Key::NumPad6 => interpretor.set_released(0x6),
+                Key::NumPad7 => interpretor.set_released(0x7),
+                Key::NumPad8 => interpretor.set_released(0x8),
+                Key::NumPad9 => interpretor.set_released(0x9),
+                Key::A => interpretor.set_released(0xA),
+                Key::B => interpretor.set_released(0xB),
+                Key::C => interpretor.set_released(0xC),
+                Key::D => interpretor.set_released(0xD),
+                Key::E => interpretor.set_released(0xE),
+                Key::F => interpretor.set_released(0xF),
                 _ => {}
             }
         );
