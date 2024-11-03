8cb8f0f117a8716aae41c68c9813b8a7cb306634
K4kug3n <maxime.rambosson@gmail.com>
2024-11-03 15:46:16 +0100

    Implement Interpretor using Display

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,7 @@
 use std::io::prelude::*;
 use std::fs::File;
 
-struct Interpretor {
-    program : Vec<u8>,
-    registers : [u8; 16],
-    l : u16
-}
-
-fn decode(op: u8) {
-    match op {
-        0x0000 => {}, // Ignored
-        0x00E0 => {},
-        _ => println!("{} not managed", op)
-    }
-}
+use chippy::interpretor::Interpretor;
 
 fn main() {
     let mut file = File::open("examples/1-chip8-logo.ch8").expect("Could not read the file {}");
@@ -22,5 +10,8 @@ fn main() {
     file.read_to_end(&mut program).expect("Could not read bytes");
 
     println!("{:#06x?}", program);
+
+
+    let interpretor = Interpretor::new(program);
 }
  
