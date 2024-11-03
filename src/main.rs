9134d1293c92df0195eb94e3b27349462842b926
K4kug3n <maxime.rambosson@gmail.com>
2024-11-03 15:46:17 +0100

    Fix opcode reading

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,19 @@ use chippy::interpretor::Interpretor;
 fn main() {
     let mut file = File::open("examples/1-chip8-logo.ch8").expect("Could not read the file {}");
     
-    let mut program = Vec::new();
-    file.read_to_end(&mut program).expect("Could not read bytes");
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).expect("Could not read bytes");
+
+    let mut program: Vec<u16> = Vec::new();
+    for i in (0..buffer.len()).step_by(2) {
+        let mut op : u16 = 0u16;
+        op += u16::from(buffer[i]) << 8;
+        op += u16::from(buffer[i+1]);
+
+        program.push(op)
+    }
 
     println!("{:#06x?}", program);
-
 
     let interpretor = Interpretor::new(program);
 }
